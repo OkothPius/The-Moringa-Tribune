7f7e14a18698e9a7f78916fa94fb1ba48d554801
OkothPius <orukopius8@gmail.com>
2019-05-07 23:22:14 +0300

    save the current changes

---
+++ news/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+    #Query first names    
+    class Meta:
+        ordering = ['first_name']    
+
+        # try:
+        #     editor = Editor.objects.get(email = 'example@gmail.com')
+        #     print('Editor found')
+        # except DoesNotExist:
+        #     print('Editor was not found')
+
+#tags model
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name            
+
+#articles model
+class Articles(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    
+
+                
